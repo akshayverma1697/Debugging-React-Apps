46cba4c0d9230259d7e4bed465c125fdbc3dbf4c
Akshay Verma <47255769+akshayverma1697@users.noreply.github.com>
2023-08-16 20:48:19 -0500

    corrected the addGoalHandler function to correctly set a id for each goal so that you can correctly delete a goal from a list

--- src/App.js
+++ src/App.js
@@ -10,10 +10,24 @@ const App = () => {
     { text: 'Finish the course!', id: 'g2' }
   ]);
 
+/* 
+  const addGoalHandler = enteredText => {
+    setCourseGoals(prevGoals => {
+      const updatedGoals = [...prevGoals];https://lh3.googleusercontent.com/TNijZW_Gp9MZ3eqXkve0YWDEiHV-a2IpSpD6IJzrV3Y76GJcLEyzX2regTLemXzBHbHVqkKuxnnWDT34Cp4sNh-Y=w128-h128-e365-rj-sc0x00ffffff
+      updatedGoals.unshift({ text: enteredText, id: 'goal1' });
+      return updatedGoals;
+    });
+  };
+  This code is incorrect because if you look at line 17 we have 
+  a static id for all goals, so when we go to delete a goal we 
+  will not delete the correct one the correct way to write 
+  this function is below
+ */
+
   const addGoalHandler = enteredText => {
     setCourseGoals(prevGoals => {
       const updatedGoals = [...prevGoals];
-      updatedGoals.unshift({ text: enteredText, id: 'goal1' });
+      updatedGoals.unshift({ text: enteredText, id: Math.random().toString() });
       return updatedGoals;
     });
   };
